a24a2216eebad3cb487854efead5364b5f99b7c1
Abhinay Jangati <abhinayjangati@carfax.com>
2018-09-17 00:58:56 -0500

    Rendered simple JSX.

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("App.js is running");
+
+var template = React.createElement(
+  "p",
+  null,
+  "This is JSX from app.js!"
+);
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
