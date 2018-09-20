663c73fc89b7bc5ff8cca52eb3f67dcf8d84a7fe
Abhinay Jangati <abhinayjangati@carfax.com>
2018-09-19 21:06:55 -0500

    Object reference in JSX expression to render content dynamically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,61 @@
-"use strict";
+'use strict';
 
 console.log("App.js is running");
 
 var template = React.createElement(
-  "p",
-  null,
-  "This is JSX from app.js!"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is JSX from app.js!!'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
+);
+
+var user = {
+    name: 'Abhinay Jangati',
+    age: 27,
+    location: 'Missouri'
+};
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 console.log("App.js is running");
 
-var template = <p>This is JSX from app.js!</p>;
+var template = 
+(
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is JSX from app.js!!</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name : 'Abhinay Jangati',
+    age : 27,
+    location : 'Missouri'
+};
+var template2 = 
+(
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
