961b240ad2f990b4a84c743bcf3373cd469777e5
Abhinay Jangati <abhinayjangati@carfax.com>
2018-09-21 14:16:42 -0500

    var, let and const variables

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 console.log("App.js is running");
 
-var app = {
+const app = {
     title : 'Indecision App',
     subtitle : 'React Tutorial',
     options :  ['One', 'Two']
 };
-var template = 
+const template = 
 (
     <div>
         <h1>{app.title}</h1>
         <p>{app.subtitle && app.subtitle}</p>
         <p>{app.options.length > 0 ? 'Here are your options': 'No options'}</p>
     </div>
-);
+)
 
-var user = {
+const user = {
     name : 'Abhinay Jangati',
     age : 27,
     location : 'Missouri'
@@ -25,7 +25,7 @@ function getLocation (location) {
         return <p>Location : {location}</p>
     }
 };
-var template2 = 
+const template2 = 
 (
     <div>
         <h1>{user.name ? user.name : 'Anonymous'}</h1>
@@ -33,5 +33,5 @@ var template2 =
         {getLocation(user.location)}
     </div>
 );
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
